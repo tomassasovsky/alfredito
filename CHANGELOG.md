b88f75aece924847cf3955b667c9b4bbd75db920
tomassasovsky <63405252+tomassasovsky@users.noreply.github.com>
2022-05-24 10:59:00 -0300

    added unique-driven queue service

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.1+1
+
+* Implemented unique-driven queues for non-parallel processes.
+
 ## 0.0.1
 
 * Implemented Alfredito extension class.
--- lib/alfredito.dart
+++ lib/alfredito.dart
@@ -1,5 +1,6 @@
 library alfredito;
 
 export 'src/alfredito.dart';
+export 'src/queue/queue_service.dart';
 export 'src/route_handler/route_handler.dart';
 export 'src/validators/validators.dart';
